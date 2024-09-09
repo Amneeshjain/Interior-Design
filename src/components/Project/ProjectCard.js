5afcf649a321be52ee20228f9fc8d6b4fb6ce392
aj-Amneesh <amneesh.jain@mediadynox.com>
2024-09-09 12:56:08 +0530

    rework

--- src/components/Project/ProjectCard.js
+++ src/components/Project/ProjectCard.js
@@ -15,7 +15,7 @@ const ProjectCard = ({ project }) =>
           <Image
             src={project.image}
             alt={project.title}
-            layout="fill"
+            layout="cover"
 
 
           />
@@ -39,7 +39,7 @@ const ProjectCard = ({ project }) =>
           <Image
             src={project.image}
             alt={project.title}
-            layout="fill"
+            layout="cover"
             className={styles.image}
           />
           <div className={styles.titleOverlay}>
@@ -64,38 +64,4 @@ const ProjectCard = ({ project }) =>
 export default ProjectCard;
 
 
-// import React from "react";
-// import Image from "next/image";
-// import styles from "../../styles/project.module.css";
-// import style from "../../styles/home.module.css";
 
-// const ProjectCard = ({ project }) => (
-//   <>
-//     <div className={styles.card}>
-//       <div className={styles.cardImageContainer}>
-//         <Image
-//           src={project.image}
-//           alt={project.title}
-//           layout="fill"
-//           className={styles.image}
-//         />
-//         <div className={styles.titleOverlay}>
-//           {project.title}
-//         </div>
-//       </div>
-
-//       <div className={styles.content}>
-//         <div className={styles.contentText}>
-//           <h2 className={`${style.fs26} ${style.fw300}`}>
-//             {project.title}
-//           </h2>
-//           <p className={`${style.fs16} ${style.fw300}`}>
-//             {project.description}
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   </>
-// );
-
-// export default ProjectCard;
